61a51893c5d506de23032c5f6dfd53ac4f32974e
nomi898 <naumanzahid898@gmail.com>
2026-03-09 13:27:20 +0500

    added deployment logic

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import path from "path";
 
 import notesRouter from "./routes/noteRoutes.js";
 import { connectDB } from "../config/db.js";
@@ -9,22 +10,33 @@ import ratelimiter from "../middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
-const PORT =process.env.PORT || 5001; 
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({
-  origin: "http://localhost:5174"
-}));
+if(process.env.NODE_ENV !=="production"){
+app.use(
+  cors({
+    origin: "http://localhost:5174",
+  })
+);
+};
+
 app.use(express.json());
 app.use(ratelimiter);
 
-
 app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get(
+    "*",
+    (req,res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("Server Started on Port:" , PORT);
+    console.log("Server Started on Port:", PORT);
   });
-})
-
-
-
+});
